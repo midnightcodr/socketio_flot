250a90d9f0f9e9a7c64c938bd0af29fb726f8dba
Rico Chen <xiakingc@hotmail.com>
2012-02-09 21:10:31 -0500

    made sourcecode easier to ready on the browser, removed useless slider change event

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function gt() {
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.set('view options', {layout:false});
+  app.set('view options', {layout:false, pretty:true});
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
